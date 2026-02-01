ede254c8d01539ce3a69ae8c436114b04d6c4a16
JoseNapitupulu <josemourinhonapitupulu@gmail.com>
2026-02-01 12:15:29 +0700

    Create main.py

---
+++ untuk Delcom/main.py
@@ -0,0 +1,13 @@
+from repositories.TodoRepository import TodoRepository
+from services.TodoService import TodoService
+from views.TodoView import TodoView
+
+def main():
+    todo_repository = TodoRepository()
+    todo_service = TodoService(todo_repository)
+    todo_view = TodoView(todo_service)
+
+    todo_view.show_todos()
+
+if __name__ == "__main__":
+    main()
